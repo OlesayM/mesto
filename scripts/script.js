0a1a9749ddce19618dc029cac1ce0a5cfcb3d5a6
Olesya M <olesayvalerevna@mail.ru>
2023-02-12 03:01:15 +0300

    второй

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,32 @@
 let editPopup =document.querySelector('.popup');
-console.log(editPopup);
 let editProfilButton =document.querySelector('.profile__editbuttom');
-console.log(editProfilButton);
 editProfilButton.addEventListener('click', function () {
-  /*console.log('Мы кликнули по элементу');*/
-  editPopup.classList.add('popup__open');
+   editPopup.classList.add('popup__open');
 });
 let closePopup =document.querySelector('.popup__close');
-console.log(closePopup);
 closePopup.addEventListener('click', function () {
-  /*console.log('Мы кликнули по элементу');*/
   editPopup.classList.remove('popup__open');
 });
+let userName = 'Жак-Ив Кусто';
+let userOccupation = 'Исследователь океана';
+let userNameElement = document.querySelector('.profile__username');
+userNameElement.textContent =userName;
+let userOccupationElement = document.querySelector('.profile__occupation');
+userOccupationElement.textContent =userOccupation;
+let userNameInput = document.querySelector('.popup__text-input_username');
+userNameInput.value = userName;
+let userOccupationInput = document.querySelector('.popup__text-input_occupation');
+userOccupationInput.value = userOccupation;
+let formElement = document.querySelector('.popup__container');
+let handleFormSubmit = function(evt){
+  evt.preventDefault();
+  inputUserName = document.querySelector(".popup__text-input_username").value;
+  inputUserOccupation = document.querySelector(".popup__text-input_occupation").value;
+  let userOccupationElement = document.querySelector('.profile__occupation');
+  userOccupationElement.textContent= inputUserOccupation;
+  let userNameElement = document.querySelector('.profile__username');
+  userNameElement.textContent= inputUserName;
+  editPopup.classList.remove('popup__open');
+  }
+formElement.addEventListener('submit',handleFormSubmit)
+
