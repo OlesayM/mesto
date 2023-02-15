45d5f25200556978fa461c6b6614ce6749c6e6d4
Olesya M <olesayvalerevna@mail.ru>
2023-02-16 00:24:18 +0300

    при

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,36 @@
-let Popup =document.querySelector('.popup');
+let popup =document.querySelector('.popup');
+console.log(popup);
 let editProfileButton =document.querySelector('.profile__editbuttom');
+console.log(editProfileButton);
+let openPopup =document.querySelector('.popup_open');
 let closePopup =document.querySelector('.popup__close');
-let userNameInput = document.querySelector('.popup__text-input_username');
-let userOccupationInput = document.querySelector('.popup__text-input_occupation');
+let userNameInput = document.querySelector('input[name="editForm_username"]');
+console.log(userNameInput);
+let userOccupationInput = document.querySelector('input[name="editForm_occupation"]');
+console.log(userOccupationInput);
 let userNameElement = document.querySelector('.profile__username');
 let userOccupationElement = document.querySelector('.profile__occupation');
-let formElement = document.querySelector('.popup__container');
+let formElement = document.querySelector('form[name="editForm"]');
+console.log(formElement);
 
-function OpenPopup_form() {
-  Popup.classList.add('popup__open');
-};
-
-function ClosePopup_form() {
-  Popup.classList.remove('popup__open');
+function openPopup_form() {
+  popup.classList.add('popup_open');
   userNameInput.value = userNameElement.textContent;
   userOccupationInput.value = userOccupationElement.textContent;
-};
+}
+
+function closePopup_form() {
+  popup.classList.remove('popup_open');
+}
 
 function handleFormSubmit(evt){
   evt.preventDefault();
   userNameElement.textContent = userNameInput.value;
   userOccupationElement.textContent = userOccupationInput.value;
-  ClosePopup_form();
+  closePopup_form();
   }
   
-editProfileButton.addEventListener('click', OpenPopup_form);
-closePopup.addEventListener('click', ClosePopup_form);
+editProfileButton.addEventListener('click', openPopup_form);
+closePopup.addEventListener('click', closePopup_form);
 formElement.addEventListener('submit', handleFormSubmit)
 
