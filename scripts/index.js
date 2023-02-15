6152a056d246d49441900c4b5fdca38084a4f85f
Olesya M <olesayvalerevna@mail.ru>
2023-02-16 01:12:00 +0300

    itter

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,12 @@
 let popup =document.querySelector('.popup');
-console.log(popup);
 let editProfileButton =document.querySelector('.profile__editbuttom');
-console.log(editProfileButton);
 let openPopup =document.querySelector('.popup_open');
 let closePopup =document.querySelector('.popup__close');
 let userNameInput = document.querySelector('input[name="editForm_username"]');
-console.log(userNameInput);
 let userOccupationInput = document.querySelector('input[name="editForm_occupation"]');
-console.log(userOccupationInput);
 let userNameElement = document.querySelector('.profile__username');
 let userOccupationElement = document.querySelector('.profile__occupation');
 let formElement = document.querySelector('form[name="editForm"]');
-console.log(formElement);
 
 function openPopup_form() {
   popup.classList.add('popup_open');
